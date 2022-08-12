380b649836318ae47df837ab50f88eb6f85850d3
Lee-GeonWoo <70263403+Lee-GeonWoo@users.noreply.github.com>
2022-08-12 16:57:12 +0900

    Create cacerts.sh

---
+++ multi-cluster/cacerts.sh
@@ -0,0 +1,20 @@
+cd
+cd istio-1.14.3
+
+mkdir -p certs
+pushd certs
+make -f ../tools/certs/Makefile.selfsigned.mk root-ca
+
+for i in $(seq "${NUM_CLUSTERS}"); do
+  make -f ../tools/certs/Makefile.selfsigned.mk "cluster${i}-cacerts"
+  kubectl create namespace istio-system --context "cluster${i}"
+  kubectl --context="cluster${i}" label namespace istio-system topology.istio.io/network="network${i}"
+  kubectl create secret generic cacerts -n istio-system --context "cluster${i}" \
+      --from-file="cluster${i}/ca-cert.pem" \
+      --from-file="cluster${i}/ca-key.pem" \
+      --from-file="cluster${i}/root-cert.pem" \
+      --from-file="cluster${i}/cert-chain.pem"
+  echo "----"
+done
+
+popd
